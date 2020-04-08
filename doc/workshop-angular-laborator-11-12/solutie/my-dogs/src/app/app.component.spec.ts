2b259ffb1943caa73f376bae63b718aa462708d6
Bogdan <bmnicolae@gmail.com>
2020-04-08 16:07:25 +0300

    Workshop Angular  - Update to Angular 9.1

--- doc/workshop-angular-laborator-11-12/solutie/my-dogs/src/app/app.component.spec.ts
+++ doc/workshop-angular-laborator-11-12/solutie/my-dogs/src/app/app.component.spec.ts
@@ -12,20 +12,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'my-dogs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('my-dogs');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-dogs!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('my-dogs app is running!');
   });
 });
